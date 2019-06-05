bde60dc5cdf738d161a9bc70a84704419a29e178
everett lee-wuollet and hannah melendy <everett@>
2019-06-05 13:24:05 -0700

    add functionality to subtract two numbers; comment out addition code.

--- js/scripts.js
+++ js/scripts.js
@@ -20,11 +20,21 @@ var divide = function(number1, number2) {
 // User interface (or front-end) logic:
 
 $(document).ready(function() {
-  $("form#add").submit(function(event) {
+  // $("form#add").submit(function(event) {
+  //   event.preventDefault();
+  //   var number1 = parseInt($("#add1").val());
+  //   var number2 = parseInt($("#add2").val());
+  //   var result = (add(number1, number2));
+  //   $("#output").text(result);
+  // });
+  $("form#subtract").submit(function(event) {
     event.preventDefault();
-    var number1 = parseInt($("#add1").val());
-    var number2 = parseInt($("#add2").val());
-    var result = (add(number1, number2));
+    var number1 = parseInt($("#minus1").val());
+    var number2 = parseInt($("#minus2").val());
+    console.log(number2);
+    var result = (subtract(number1, number2));
+    console.log(result);
     $("#output").text(result);
+
   });
 });
